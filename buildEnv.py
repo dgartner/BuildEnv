a6a93dff4ad95987e1c485ff1e3d225cfbdd6dd0
dgartner <david.gartner@gmail.com>
2014-04-17 08:38:09 -0500

    add method structure

--- buildEnv.py
+++ buildEnv.py
@@ -3,21 +3,24 @@ import os
 import sys
 import zipfile
 
-num_args = len(sys.argv)
-
-if num_args == 1:
-    archive = "D:\\archive44\\"
-elif num_args > 1:
-    ver = sys.argv[1]
-    if int(ver) == 40:
+def unzip_products(version=44):
+    if int(version) == 40:
         archive = "D:\\archive40\\"
         env = "D:\\osi40_test\\monarch"
-    elif int(ver) == 44:
+    elif int(version) == 44:
         archive = "D:\\archive44\\"
         env = "D:\\osi44_test\\monarch"
 
-for zip_name in os.listdir(archive):
-    zip = zipfile.ZipFile(archive + zip_name)
-    zip.extractall(env)
+    for zip_name in os.listdir(archive):
+        zip = zipfile.ZipFile(archive + zip_name)
+        zip.extractall(env)
             
+if __name__ == "__main__":
+    num_args = len(sys.argv)
+    if num_args != 2:
+        print ('Please specify version number')
+        exit(0)
+    else:
+        version = int(sys.argv[1])
+    unzip_products(version)
 
